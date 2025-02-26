caa106819a10ba36a2df2c18c549c406ba6e4c5d
Artem Posudevskyi <penquin4m@gmail.com>
2025-02-27 01:35:52 +0200

    fixes

--- src/mainPage/MainPage.jsx
+++ src/mainPage/MainPage.jsx
@@ -31,7 +31,6 @@ function MainPage() {
             try {
                 const response = await fetch(url);
                 const result = await response.json();
-                console.log(result);
                 setCountries(result);
             } catch (error) {
                 console.error(error);
@@ -53,49 +52,29 @@ function MainPage() {
       return () => window.removeEventListener('resize', PhoneFunc);
     }, []);
     
+    const scrollToCountries = () => {
+      document.getElementById("country-section").scrollIntoView({ behavior: "smooth" });
+    };
+    
     const commonCountries = !inputValue ? countries : searchCountries;
     return (
       <div className="bg-gray-50 min-h-screen flex flex-col">
-      {/* Header */}
       <header className="bg-white shadow-md sticky top-0 z-50">
         <div className="container mx-auto flex justify-between items-center py-4 px-6">
           <h1 className="text-2xl font-bold text-gray-800">Country Explorer</h1>
           <nav>
-            {isMobile ? (<ul className="flex space-x-6">
+            {isMobile ? (
               <button className="text-3xl" onClick={HandleToggle}>☰</button>
-            </ul>) : (<ul className="flex space-x-6">
-              <li className="text-gray-700 hover:text-blue-500 transition cursor-pointer" id="#">Home</li>
-              <li className="text-gray-700 hover:text-blue-500 transition cursor-pointer" id="#country">Countries</li>
-              <a className="text-gray-700 hover:text-blue-500 transition cursor-pointer" href="https://github.com/4twerka">My Github</a>
-            </ul>)}
+            ) : (
+              <ul className="flex space-x-6">
+                <li className="text-gray-700 hover:text-blue-500 transition cursor-pointer" onClick={scrollToCountries}>Countries</li>
+                <a className="text-gray-700 hover:text-blue-500 transition cursor-pointer" href="https://github.com/4twerka">My Github</a>
+              </ul>
+            )}
           </nav>
         </div>
       </header>
 
-      {isToggle ? (
-      <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-        <nav className="bg-white rounded-lg p-10 relative z-50">
-          <button className="absolute top-2 right-0 text-red-700 px-4 py-2 rounded z-60" onClick={HandleToggle}>
-          ❌
-          </button>
-          <ul className="flex flex-col space-y-8 text-center text-gray-800">
-            <li>
-              <a href="#" className="text-2xl hover:text-gray-500">Home</a>
-            </li>
-            <li>
-              <a href="#country" className="text-2xl hover:text-gray-500">Countries</a>
-            </li>
-            <li>
-              <a href="https://github.com/4twerka" className="text-2xl hover:text-gray-500">GitHub</a>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    ) : (
-      <div className="object-none"></div>
-    )}
-
-      {/* Hero Section */}
       <section className="bg-cover bg-center h-screen" style={{ backgroundImage: 'url(https://i.pinimg.com/originals/99/f9/5e/99f95ee41c3def28268cc3877f103daf.gif)' }}>
         <div className="bg-black bg-opacity-50 h-full flex items-center justify-center">
           <h2 className="text-4xl md:text-6xl font-extrabold text-white text-center">
@@ -104,24 +83,20 @@ function MainPage() {
         </div>
       </section>
 
-      {/* Countries Section */}
       {isLoading ? (<div className="flex justify-center text-5xl text-black items-center">Loading...</div>) : (
-          <section className="py-16 bg-white">
+          <section id="country-section" className="py-16 bg-white">
           <div className="container mx-auto px-6">
             <h3 className="text-3xl font-bold text-gray-800 text-center mb-10">Featured Countries</h3>
             <div className="flex justify-center items-center">
-            <input type="text"
-             className="pl-4 py-2 border-2 border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 transition-all w-full sm:w-96"
-              placeholder="Search country" value={inputValue} onChange={HandleInputValue}/>
-
+              <input type="text" className="pl-4 py-2 border-2 border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 transition-all w-full sm:w-96" placeholder="Search country" value={inputValue} onChange={HandleInputValue}/>
             </div>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10" id="country">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10">
                   {commonCountries.length > 0 ? (
                     commonCountries.map((country, index) => {
                       return (
                           <div key={index} className="bg-gray-100 p-6 rounded-lg shadow-lg">
                               <a href={country.maps.googleMaps}>
-                              <img src={country.flags.png} alt="France" className="w-full h-48 object-cover rounded-md mb-4" />
+                              <img src={country.flags.png} alt={country.name.common} className="w-full h-48 object-cover rounded-md mb-4" />
                               <h4 className="text-xl font-bold text-gray-800">{country.name.common}</h4>
                               <p className="text-gray-600 mt-2">{country.continents}</p>
                               </a>
@@ -136,7 +111,6 @@ function MainPage() {
         </section>
       )}
 
-      {/* Footer */}
       <footer className="bg-gray-800 text-white py-8">
         <div className="container mx-auto px-6 text-center">
           <p className="text-sm">© 2024 World Explorer. All Rights Reserved.</p>
